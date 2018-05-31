4d64598bc34f748dbe1a0ffcf63fb898844ab38a
r0rshark <lord.fontana@gmail.com>
2018-05-31 11:44:20 -0700

    fixed merge problem
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- angr_targets/__init__.py
+++ angr_targets/__init__.py
@@ -1,16 +1,6 @@
 from .concrete import ConcreteTarget
-<<<<<<< HEAD
 from .targets.avatar_gdb import AvatarGDBConcreteTarget
-
 try:
  from .targets.ida_gui import IDAConcreteTarget
 except Exception:
     pass
-
-=======
-
-try:
-    from .targets.ida_gui import IDAConcreteTarget
-except Exception:
-    pass
->>>>>>> 4e927e55700aa1d35982b5b1af4ccd00a1c26818
