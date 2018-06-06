d329f49b8e178a6d5bd80c815951b9a359401d70
r0rshark <lord.fontana@gmail.com>
2018-06-06 15:14:19 -0700

    made the installation of target not mandatory
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- angr_targets/__init__.py
+++ angr_targets/__init__.py
@@ -1,6 +1,8 @@
 from .concrete import ConcreteTarget
-from .targets.avatar_gdb import AvatarGDBConcreteTarget
-try:
- from .targets.ida_gui import IDAConcreteTarget
+# The availability of the target depends on the environment in which angr is installed
+# For example on the idaPython interpreter the IDAConcreteTarget is available but not the AvatarGDBConcreteTarget 
+try: 
+  from .targets.avatar_gdb import AvatarGDBConcreteTarget
+  from .targets.ida_gui import IDAConcreteTarget
 except Exception:
     pass
