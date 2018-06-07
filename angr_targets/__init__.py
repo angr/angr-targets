3886ee3c22e4dab7780031c632f3e2d437c08fd4
r0rshark <lord.fontana@gmail.com>
2018-06-07 14:11:12 -0700

    divided import targets
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- angr_targets/__init__.py
+++ angr_targets/__init__.py
@@ -1,8 +1,16 @@
 from .concrete import ConcreteTarget
+import logging
+l = logging.getLogger("angr_targets.init")
+#l.setLevel(logging.DEBUG)
 # The availability of the target depends on the environment in which angr is installed
 # For example on the idaPython interpreter the IDAConcreteTarget is available but not the AvatarGDBConcreteTarget 
 try: 
   from .targets.avatar_gdb import AvatarGDBConcreteTarget
-  from .targets.ida_gui import IDAConcreteTarget
 except Exception:
-    pass
+    l.info("Impossible to load AvatarGDBConcreteTarget")
+
+
+try:
+    from .targets.ida_gui import IDAConcreteTarget
+except Exception:
+    l.info("Impossible to load IDAConcreteTarget")
