adc2964ca94200857d3b83a048cff21d80c3b4be
r0rshark <lord.fontana@gmail.com>
2018-06-14 16:58:23 -0700

    added check for platform install
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import sys
 try:
     from setuptools import setup
     from setuptools import find_packages
 except ImportError:
     from distutils.core import setup
 
-setup(name='angr_targets',
-      version='0.1',
-      packages=['angr_targets'],
-      )
+if sys.platform in ('win32', 'cygwin', 'darwin'):
+    setup(name='angr_targets',
+          version='0.1',
+          packages=['angr_targets'],
+         )
+else:
+    setup(name='angr_targets',
+          version='0.1',
+          packages=['angr_targets'],
+          install_requires=['avatar2']
+          )
