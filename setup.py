a2d0443333f0291bfbe7a9fda3588c197c97c856
r0rshark <lord.fontana@gmail.com>
2018-06-15 16:31:41 -0700

    modified setup.py to include submodules
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 import sys
+import os
 try:
     from setuptools import setup
     from setuptools import find_packages
+    packages = find_packages()
 except ImportError:
     from distutils.core import setup
-
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
 if sys.platform in ('win32', 'cygwin', 'darwin'):
     setup(name='angr_targets',
           version='0.1',
-          packages=['angr_targets'],
+          packages=packages
          )
 else:
     setup(name='angr_targets',
           version='0.1',
-          packages=['angr_targets'],
-	  dependency_links=['https://github.com/avatartwo/avatar2/tarball/master#egg=avatar2-1.1.1'],
-	  install_requires=['avatar2']
+          packages=packages,
+	      dependency_links=['https://github.com/avatartwo/avatar2/tarball/master#egg=avatar2-1.1.1'],
+	      install_requires=['avatar2']
           )
