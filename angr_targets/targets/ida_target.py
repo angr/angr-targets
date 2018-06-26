f0ebb68b87a02b9ab56a52218566e13d41b88689
r0rshark <lord.fontana@gmail.com>
2018-06-26 15:42:58 -0700

    fixed bug in get_mappings
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- angr_targets/targets/ida_target.py
+++ angr_targets/targets/ida_target.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import os
 
 import idaapi
 import idc
@@ -468,10 +469,10 @@ class IDAConcreteTarget(ConcreteTarget):
             Describing a memory range inside the concrete
             process.
             """
-            def __init__(self, start_address, end_address, offset, name):
+            def __init__(self, start_address, end_address, name):
                 self.start_address = start_address
                 self.end_address = end_address
-                self.name = name
+                self.name = os.path.basename(name)
 
             def __str__(self):
                 my_str = "MemoryMap[start_address: 0x%x | end_address: 0x%x | name: %s" \
